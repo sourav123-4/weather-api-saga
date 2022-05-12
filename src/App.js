f64ee593220b8705ea9282e8e0bee59ad77bcbb4
sourav12 <souravmahanty12@hmail.com>
2022-05-12 14:57:51 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Button } from '@material-ui/core';
 function App() {
   const [search,setSearch]=React.useState("");
   const state = useSelector(state=>state.data);
-  const [value,setValue]=React.useState("kolkata")
+  const [value,setValue]=React.useState("")
   let dispatch=useDispatch();
   React.useEffect(()=>{
     dispatch({type:REQUEST_API_DATA,payload:value});
@@ -16,25 +16,33 @@ function App() {
     setValue(search)
     setSearch("");
   }
-  console.log("state", state)
   return (
-    <div className={state?.main?.temp_max-273 > 30 ? "App_hot" : "App_cold"}>
-      <h1 style={{padding:"10px"}}>Weather App</h1>
+    <div className={!state.weather?"common":state?.main?.temp_max-273 > 30 ? "App_hot" : "App_cold"}>
+      <h1 style={{padding:"10px",fontSize:"70px"}}>Weather App</h1>
       <form>
         <TextField 
           id="outlined-basic" 
           variant="outlined" 
           label="City" 
           value={search}
-          onChange={(e)=>setSearch(e.target.value)}/>
-          <Button  variant="outlined" onClick={updateData} color='primary' style={{height:"50px",color:"aquamarine"}}>Search</Button>
+          onChange={(e)=>setSearch(e.target.value)}
+        />
+        <Button  
+          variant="outlined" 
+          onClick={updateData} 
+          color='primary' 
+          style={{height:"56px",color:"white",backgroundColor:"",marginLeft:"7px"}}
+          >Search</Button>
       </form>
       {state.weather &&
         <div className='data'>
-          <h1 style={{color:"violet"}}>{state?.name}</h1>
-          <h3>{(state?.main?.temp_max-273)?.toFixed(2)}</h3>
-           <h3>{(state?.main?.temp_min-273)?.toFixed(2)}</h3>
-          <h3>{state?.weather[0]?.main}</h3>
+          <h1 style={{color:"Highlight",fontSize:"80px"}}>{state?.name}</h1>
+          <h3 style={{color:"blue",fontSize:"50px"}}>{state?.weather[0]?.main}</h3>
+          <div className='temp'>
+            <h3 style={{color:"aliceblue",fontSize:"40px"}}>{(state?.main?.temp_min-273)?.toFixed(2)}°C</h3>
+            <h1>----</h1>
+            <h3 style={{color:"HighlightText",fontSize:"40px"}}>{(state?.main?.temp_max-273)?.toFixed(2)}°C</h3>
+          </div>
         </div>}
     </div>
   );
